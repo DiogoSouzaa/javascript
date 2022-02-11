14432a52592d0e849ee1df04862c335913fd4d10
Diogo Souza <diogosouza19@gmail.com>
2022-02-10 21:07:33 -0300

    aula 12 e 13 exercicio

---
+++ aula 012/ex012.js
@@ -0,0 +1,19 @@
+/*var hora = 13
+console.log(`Agora são exatamente ${hora} horas`)
+if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}*/
+var agora = new Date()
+var hora = agora.getHours()
+console.log(`Agora são exatamente ${hora} horas`)
+if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else {
+    console.log('Boa Noite!')
+}
