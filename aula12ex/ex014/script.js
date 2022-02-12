ecdbe14ee84dd9d77e9b9094cc3f8d10bddc2033
Diogo Souza <diogosouza19@gmail.com>
2022-02-12 17:02:04 -0300

    finalização exercicio hora do dia

--- aula12ex/ex014/script.js
+++ aula12ex/ex014/script.js
@@ -5,10 +5,13 @@ function carregar(){
      var hora = data.getHours()
      msg.innerHTML = `Agora são ${hora} horas.`
      if (hora >= 0 && hora < 12) {
-         img.src = 'imagens/manhã_p.jpg'
+         img.src = 'imagens/manha.png'
+         document.body.style.background = '#e2cd9f'
      } else if (hora >= 12 && hora < 18) {
-         img.src = 'imagens/tarde_p.jpg'
+         img.src = 'imagens/tarde.png'
+         document.body.style.background = '#b9846f'
      } else {
-         img.src = 'imagens/noite_p.jpg'
+         img.src = 'imagens/noite.png'
+         document.body.style.background = '#515154'
      }
 }
